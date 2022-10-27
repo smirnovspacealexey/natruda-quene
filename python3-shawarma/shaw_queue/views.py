d2cee253e9e8d13a04ad023381b6ee5aba3413bd
smirnovspacealexey <smirnovspacealexey@yandex.ru>
2022-10-27 05:00:25 +0500

    add point in register_customer_order

--- python3-shawarma/shaw_queue/views.py
+++ python3-shawarma/shaw_queue/views.py
@@ -2846,7 +2846,7 @@ def delivery_interface(request):
     logger_debug = logging.getLogger('debug_logger')  # del me
     logger_debug.info(f'delivery_interface\n{delivery_orders}\n{deliveries}\n{timezone.datetime.today().date()}\n\n')  # del me
     logger_debug.info(f'delivery_orders2\n{delivery_orders2}\n\n')  # del me
-    last_order = delivery_orders2.last()
+    last_order = delivery_orders2.filter(pk=1161).last()
     logger_debug.info(f'{last_order.pk}\n{last_order.obtain_timepoint}\n{last_order.obtain_timepoint.date()==timezone.datetime.today().date()}\n\n')  # del me
 
 
