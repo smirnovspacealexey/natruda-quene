1cc5cdb7cdbe3280da12fca365ef23dae17368ed
smirnovspacealexey <smirnovspacealexey@yandex.ru>
2022-11-09 14:18:07 +0500

    add loggs

--- python3-shawarma/shaw_queue/views.py
+++ python3-shawarma/shaw_queue/views.py
@@ -6145,6 +6145,9 @@ def log_deleted_order(order):
 
 def status_refresher(request):
     order_guid = request.POST.get('order_guid', None)
+    import logging
+    logger_debug = logging.getLogger('debug_logger')
+    logger_debug.info(f'order_guid {order_guid}')
     if order_guid is not None:
         try:
             order = Order.objects.get(guid_1c=order_guid)
