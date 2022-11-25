47984b1aadb8a61be7dd0dd3fe0120be1dd08117
smirnovspacealexey <smirnovspacealexey@yandex.ru>
2022-11-25 11:38:23 +0500

    add cooking timer

--- python3-shawarma/shaw_queue/management/commands/cooking_timer.py
+++ python3-shawarma/shaw_queue/management/commands/cooking_timer.py
@@ -10,7 +10,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
        print('--------START---------')
-       logger_debug.info(f'!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+
+       delivery_orders = DeliveryOrder.objects.filter(obtain_timepoint__gte=timezone.datetime.today().date(),
+                                                      order__close_time__isnull=True).order_by('delivered_timepoint')
+       logger_debug.info(f'delivery_orders{timezone.datetime.today()} \n{delivery_orders}')
+       for delivery_order in delivery_orders:
+          for order in delivery_order.order.ordercontent_set.all():
+              logger_debug.info(f'{order} {order.menu_item.get_cooking_time()}')
        print('this')
 
        print('---------END----------')
