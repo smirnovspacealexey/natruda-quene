18548395acac41cabe0abadd28828b84795a9fc1
smirnovspacealexey <smirnovspacealexey@yandex.ru>
2023-02-26 09:23:29 +0500

    add qrs

--- python3-shawarma/shaw_queue/static/js/menu_handlers.js
+++ python3-shawarma/shaw_queue/static/js/menu_handlers.js
@@ -726,7 +726,7 @@ function ShowModalEdit(index) {
     modal.style.display = "block";
 
     if (currOrder[index]['qr_req']) {
-        title.append('<sub style="margin-left: 20px;  color: rgba(246,96,2,0.76);">QR обязателен у этого товара</sub>');
+        title.append('<span style="margin-left: 20px; color: rgba(246,96,2,0.76); font-size: 11pt;">QR обязателен у этого товара</span>');
         qr.focus();
     } else {
         note.focus()
