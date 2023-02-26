a207388c627ee27789fd02712e497b317bdf8ef8
smirnovspacealexey <smirnovspacealexey@yandex.ru>
2023-02-26 08:51:17 +0500

    add qrs

--- python3-shawarma/shaw_queue/models.py
+++ python3-shawarma/shaw_queue/models.py
@@ -146,9 +146,10 @@ class Menu(models.Model):
         except:
             return 15
 
-    # def js_qr_req(self):
-    #     if self.QR_required:
-    #         return 'true'
+    def js_qr_req(self):
+        if self.QR_required:
+            return 'true'
+        return 'false'
 
     def __str__(self):
         return u"{}".format(self.title)
