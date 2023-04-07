b73018ec8e46fa6d0e8479c3fecc140eb89b7f92
smirnovspacealexey <smirnovspacealexey@yandex.ru>
2023-04-07 10:39:57 +0500

    add delivery

--- python3-shawarma/apps/delivery/admin.py
+++ python3-shawarma/apps/delivery/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import YandexSettings
+from .models import YandexSettings, DeliveryHistory
 from time import gmtime, strftime
 
 
@@ -8,6 +8,11 @@ class YandexSettingsAdmin(admin.ModelAdmin):
     list_display = ['__str__', 'token', 'active']
     list_editable = ('token', 'active')
     search_fields = ['__str__', ]
+
+
+@admin.register(DeliveryHistory)
+class DeliveryHistoryAdmin(admin.ModelAdmin):
+    list_display = ['uuid', 'six_numbers']
 
 
 
