acf58c0ed2a528692776c2a8bdea477f1f30aa8a
smirnovspacealexey <smirnovspacealexey@yandex.ru>
2023-04-20 18:46:30 +0500

    add storage in delivery

--- python3-shawarma/shaw_queue/views.py
+++ python3-shawarma/shaw_queue/views.py
@@ -6949,8 +6949,7 @@ def api_delivery(request):
                 else:
                     raise ConnectionError
 
-                # success, result = send_sms(phone, f'#{daily_number}. {full_price}р. Ссылка на оплату {sber_url}')
-                success = True
+                success, result = send_sms(phone, f'#{daily_number}. {full_price}р. Ссылка на оплату {sber_url}')
                 if success:
                     JsonResponse(data)
                 else:
