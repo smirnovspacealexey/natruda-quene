3f5555e6b85b3700f9423a17df8071f080e93fa8
smirnovspacealexey <smirnovspacealexey@yandex.ru>
2023-04-23 21:46:03 +0500

    add loggers

--- python3-shawarma/apps/sber/views.py
+++ python3-shawarma/apps/sber/views.py
@@ -16,11 +16,13 @@ def sber_result(request):
     logger_debug.info(f'sber_result \n{request.GET}')
     if daily_number:
         order = Order.objects.filter(open_time__contains=timezone.now().date(),
-                                     close_time__isnull=True, is_canceled=False, is_paid=False,
+                                     close_time__isnull=True, is_canceled=False, is_paid=True,
                                      is_ready=False, is_delivery=True, delivery_daily_number=int(daily_number)).last()
         order.is_paid = True
         order.save()
-        data = send_order_to_1c(order, False)
+        # data = send_order_to_1c(order, False)
+        data = None
+
         delivery_history = order.deliveryhistory_set.last()
 
         delivery_confirm(delivery_history)
