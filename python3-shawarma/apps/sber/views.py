975d5e40e1b0a7406b1641e80436d482db53fb44
smirnovspacealexey <smirnovspacealexey@yandex.ru>
2023-04-24 11:08:26 +0500

    add loggers

--- python3-shawarma/apps/sber/views.py
+++ python3-shawarma/apps/sber/views.py
@@ -6,6 +6,7 @@ from shaw_queue.views import send_order_to_1c
 from apps.delivery.backend import delivery_confirm
 import logging
 from django.views.decorators.csrf import csrf_exempt
+import sys, traceback
 
 logger_debug = logging.getLogger('debug_logger')
 
@@ -14,20 +15,22 @@ logger_debug = logging.getLogger('debug_logger')
 def sber_result(request):
     daily_number = request.GET.get('daily_number')
     logger_debug.info(f'sber_result \n{request.GET}')
-    if daily_number:
-        order = Order.objects.filter(open_time__contains=timezone.now().date(),
-                                     close_time__isnull=True, is_canceled=False, is_paid=False,
-                                     is_ready=False, is_delivery=True, delivery_daily_number=int(daily_number)).last()
-        order.is_paid = True
-        order.save()
-        # data = send_order_to_1c(order, False)
-        data = None
+    try:
+        if daily_number:
+            order = Order.objects.filter(open_time__contains=timezone.now().date(),
+                                         close_time__isnull=True, is_canceled=False, is_paid=False,
+                                         is_ready=False, is_delivery=True, delivery_daily_number=int(daily_number)).last()
+            order.is_paid = True
+            order.save()
+            # data = send_order_to_1c(order, False)
+            data = None
 
-        delivery_history = order.deliveryhistory_set.last()
+            delivery_history = order.deliveryhistory_set.last()
 
-        delivery_confirm(delivery_history)
+            delivery_confirm(delivery_history)
 
-        logger_debug.info(f'sber_result order \n{order}\n {data}')
+            logger_debug.info(f'sber_result order \n{order}\n {data}')
 
-    return JsonResponse(data={'success': True})
-
+        return JsonResponse(data={'success': True})
+    except:
+        logger_debug.info(f'sber_result ERROR: {traceback.format_exc()}')
