3558b9f82e4d374e44f755e089c6868b46e1af40
smirnovspacealexey <smirnovspacealexey@yandex.ru>
2023-06-16 10:36:22 +0500

    add timer for delivery waitng

--- python3-shawarma/shaw_queue/management/commands/check_delivery_statuses.py
+++ python3-shawarma/shaw_queue/management/commands/check_delivery_statuses.py
@@ -17,44 +17,47 @@ class Command(BaseCommand):
         logger_debug.info(f'Check delivery statuses\n\n')
 
         for delivery in DeliveryActive.objects.all():
-            if delivery.delivery.order and delivery.delivery.order.close_time:
-                delivery.delete()
-                continue
-
-            if delivery.delivery.wait_minutes:
-                history = delivery.delivery
-                wait_minutes = delivery.delivery.wait_minutes - 5
-                logger_debug.info(wait_minutes)
-
-                if wait_minutes < 1:
-                    destination = {}
-                    destination["fullname"] = history.fullname
-                    destination['phone'] = history.phone
-                    destination['longitude'] = float(history.longitude)
-                    destination['latitude'] = float(history.latitude)
-                    destination['city'] = history.city
-                    destination['comment'] = history.comment
-                    destination['country'] = history.country
-                    destination['description'] = history.description
-                    destination['door_code'] = history.door_code if history.door_code else ''
-                    destination['porch'] = history.porch if history.porch else ''
-                    destination['sflat'] = history.sflat if history.sflat else ''
-                    destination['sfloor'] = history.sfloor if history.sfloor else ''
-
-                    items = history.items
-                    items = items.replace("\'", "\"")
-                    items = list(json.loads(items))
-                    logger_debug.info(history)
-                    history.wait_minutes = None
-                    history.save()
-                    daily_number, six_numbers = delivery_request(ServicePoint.objects.filter(id=2).last(), destination, history=history, items=items, price=history.full_price)
-                    if daily_number and history.order and history.order.is_paid:
-                        delivery_confirm(history)
-                        logger_debug.info(f'{history}\nOKAY')
+            try:
+                if delivery.delivery.order and delivery.delivery.order.close_time:
+                    delivery.delete()
                     continue
 
-                history.wait_minutes = wait_minutes
-                history.save()
-            else:
-                check_delivery_status(delivery.delivery)
+                if delivery.delivery.wait_minutes:
+                    history = delivery.delivery
+                    wait_minutes = delivery.delivery.wait_minutes - 5
+                    logger_debug.info(wait_minutes)
+
+                    if wait_minutes < 1:
+                        destination = {}
+                        destination["fullname"] = history.fullname
+                        destination['phone'] = history.phone
+                        destination['longitude'] = float(history.longitude)
+                        destination['latitude'] = float(history.latitude)
+                        destination['city'] = history.city
+                        destination['comment'] = history.comment
+                        destination['country'] = history.country
+                        destination['description'] = history.description
+                        destination['door_code'] = history.door_code if history.door_code else ''
+                        destination['porch'] = history.porch if history.porch else ''
+                        destination['sflat'] = history.sflat if history.sflat else ''
+                        destination['sfloor'] = history.sfloor if history.sfloor else ''
+
+                        items = history.items
+                        items = items.replace("\'", "\"")
+                        items = list(json.loads(items))
+                        logger_debug.info(history)
+                        history.wait_minutes = None
+                        history.save()
+                        daily_number, six_numbers = delivery_request(ServicePoint.objects.filter(id=2).last(), destination, history=history, items=items, price=history.full_price)
+                        if daily_number and history.order and history.order.is_paid:
+                            delivery_confirm(history)
+                            logger_debug.info(f'{history}\nOKAY')
+                        continue
+
+                    history.wait_minutes = wait_minutes
+                    history.save()
+                else:
+                    check_delivery_status(delivery.delivery)
+            except:
+                continue
 
