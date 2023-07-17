17e68ef2d4dbbec541b1e88e3a036d576e3781b7
smirnovspacealexey <smirnovspacealexey@yandex.ru>
2023-07-17 11:47:29 +0500

    change test7

--- python3-shawarma/shaw_queue/management/commands/test7.py
+++ python3-shawarma/shaw_queue/management/commands/test7.py
@@ -38,6 +38,9 @@ class Command(BaseCommand):
                 auth=(SERVER_1C_USER.encode('utf8'), SERVER_1C_PASS),
                 json=order_dict)
             print(result)
+            print(result.text)
+            print(result.content)
+            print(result.encoding)
             print(result.json())
         except:
             print(f'ERROR: {traceback.format_exc()}')
