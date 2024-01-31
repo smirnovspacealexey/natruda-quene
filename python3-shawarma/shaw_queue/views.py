ef32f2ad007363d7426d490d2c89262caf91e97a
smirnovspacealexey <smirnovspacealexey@yandex.ru>
2024-01-31 09:41:35 +0500

    add new orders

--- python3-shawarma/shaw_queue/views.py
+++ python3-shawarma/shaw_queue/views.py
@@ -1836,8 +1836,8 @@ def order_history_new(request):
                                                # open_time__contains=timezone.now().date(),
                                                # close_time__isnull=False,
                                                is_canceled=False, is_ready=True,
-                                               # servery__service_point=ServicePoint.objects.filter(pk=2).first()).order_by('-open_time')[:20]  #  del me
-                                               servery__service_point=result['service_point']).order_by('-open_time')[:20]
+                                               servery__service_point=ServicePoint.objects.filter(pk=2).first()).order_by('-open_time')[:20]  #  del me
+                                               # servery__service_point=result['service_point']).order_by('-open_time')[:20]
 
             # open_orders = Order.objects.all()
             print(open_orders)
